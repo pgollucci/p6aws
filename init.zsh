747768bfba001fc42274fdb133de7dffcc3327dc
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-11-07 20:58:40 -0500

    depends, gen, dry

--- init.zsh
+++ init.zsh
@@ -1,9 +1,10 @@
 p6df::modules::p6aws::version() { echo "0.0.1"; }
-
+p6df::modules::p6aws::deps() { ModuleDeps=(p6m7g8/p6types) }
 p6df::modules::p6aws::init() {
 
   local dir="$P6_DFZ_SRC_DIR/p6m7g8/p6aws"
 
-  p6df::util::path_if "$dir/bin"
   p6_bootstrap "$dir"
+
+  p6_aws_cfg__generate
 }
