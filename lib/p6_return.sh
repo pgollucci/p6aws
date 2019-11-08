f7981cc8ab7ef7f98ee4bcd67fb7a7e0c561ee2f
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-11-07 20:59:55 -0500

    returns

--- lib/p6_return.sh
+++ lib/p6_return.sh
@@ -3,24 +3,29 @@
 p6_return_aws_arn() {
     local arn="$1"
 
-    p6__return "$arn"
+    p6_return_str "$arn"
 }
 
 p6_return_aws_account_id() {
     local account_id="$1"
 
-    p6__return "$account_id"
+    p6_return_str "$account_id"
 }
 
 p6_return_aws_resource_id() {
     local resource_id="$1"
 
-    p6__return "$resource_id"
+    p6_return_str "$resource_id"
 }
 
 p6_return_aws_logical_id() {
     local logical_id="$1"
 
-    p6__return "$logical_id"
+    p6_return_str "$logical_id"
 }
 
+p6_return_aws_cfg() {
+    local cfg="$1"
+
+    p6_return_obj "$cfg"
+}
