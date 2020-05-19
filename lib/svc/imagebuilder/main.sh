7f545e2550ac9428edd926903f316ee461b9d543
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-05-19 15:15:12 -0400

    feature(imagebuilder): add svc wrappers

---
+++ lib/svc/imagebuilder/main.sh
@@ -0,0 +1,39 @@
+p6_aws_imagebuilder_svc_ic_list() {
+
+    p6_aws_cmd imagebuilder \
+	       list-infrastructure-configurations \
+	       --output text \
+	       --query "'infrastructureConfigurationSummaryList[].[Arn, Name]'"
+}
+
+p6_aws_imagebuilder_svc_ir_list() {
+
+    p6_aws_cmd imagebuilder \
+	       list-image-recipes \
+	       --output text \
+	       --query "'imageRecipeSummaryList[]'"
+}
+
+p6_aws_imagebuilder_svc_dc_list() {
+
+    p6_aws_cmd imagebuilder \
+	       list-distribution-configurations \
+	       --output text \
+	       --query "'distributionConfigurationSummaryList[]'"
+}
+
+p6_aws_imagebuilder_svc_pipelines_list() {
+
+    p6_aws_cmd imagebuilder \
+	       list-image-pipelines \
+	       --output text \
+	       --query "'imagePipelineList[].[status, platform, dateCreated, dateUpdated, name, description, arn]'"
+}
+
+p6_aws_imagebuilder_svc_images_list() {
+
+    p6_aws_cmd imagebuilder \
+	       list-images \
+	       --output text \
+	       --query "'imageVersionList[].[version, platform, owner, dateCreated, name, arn]'"
+}
