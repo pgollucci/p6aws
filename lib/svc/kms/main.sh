0afbe54c70c3864f6c2cad0890b1ec3deb40c4d6
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-05-22 15:51:06 -0400

    feature(kms): adds list functions

--- lib/svc/kms/main.sh
+++ lib/svc/kms/main.sh
@@ -5,14 +5,29 @@
 #
 #>
 ######################################################################
+p6_aws_kms_svc_list_aliases_mine() {
+
+    p6_aws_cmd kms list-aliases \
+	       --output text \
+		   --query "'Aliases[].[AliasName, AliasArn]'" | grep -v alias/aws/ | \
+		   sort
+}
+
+p6_aws_kms_svc_list_aliases_aws() {
+
+    p6_aws_cmd kms list-aliases \
+	       --output text \
+		   --query "'Aliases[].[AliasName, AliasArn]'" | grep alias/aws/ | \
+		   sort
+}
+
 p6_aws_kms_svc_list_aliases() {
 
     p6_aws_cmd kms list-aliases \
 	       --output text \
-	       --query "Aliases[]" | \
-	sort
+		   --query "'Aliases[].[AliasName, AliasArn]'" | \
+		   sort
 }
-
 
 ######################################################################
 #<
