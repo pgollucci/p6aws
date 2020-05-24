0321984ae98a3dc0dca6b21e350225760286ffbd
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-05-23 23:30:45 -0400

    chore(prompt): standardize on _prompt_info (#3)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- lib/svc/cdk/main.sh
+++ lib/svc/cdk/main.sh
@@ -1,6 +1,9 @@
 p6_aws_cdk_prompt_info() {
 
+    local str
     if ! p6_string_blank "$CDK_DEFAULT_ACCOUNT"; then
-      echo "CDK: [CDA=$CDK_DEFAULT_ACCOUNT] [CDR=$CDK_DEFAULT_REGION]"
+      str="CDK: [CDA=$CDK_DEFAULT_ACCOUNT] [CDR=$CDK_DEFAULT_REGION]"
     fi    
+
+    p6_return_str "$str"
 }
