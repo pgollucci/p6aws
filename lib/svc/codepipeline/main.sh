cecb978702c9d1bf67784c45374cd44d91765891
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-05-31 13:44:03 -0400

    feat(codepipeline): adds list, get, describe apis (#11)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ lib/svc/codepipeline/main.sh
@@ -0,0 +1,7 @@
+p6_aws_codepipeline_svc_list() {
+
+    p6_aws_cmd codepipeline \
+	       list-pipelines \
+	       --output text \
+	       --query "'pipelines[].[name, version, created, updated]'"
+}
