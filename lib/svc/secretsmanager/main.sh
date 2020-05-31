07f4461177da1342ed754c42245831417dcc8947
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-05-31 13:39:56 -0400

    feat(secretsmanager): adds list, get, describe apis

---
+++ lib/svc/secretsmanager/main.sh
@@ -0,0 +1,8 @@
+p6_aws_secretsmanager_svc_list() {
+
+    p6_aws_cmd secretsmanager \
+	       list-secrets \
+	       --output text \
+	       --query "'SecretList[].[Name, ARN, LastChangedDate, LastAccessedDate]'"
+
+}
