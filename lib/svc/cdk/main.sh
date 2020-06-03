e821672b818c7a62bb1a9ea20f2949c244fd3d1b
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-06-03 19:37:57 -0400

    feat(prompt): stdizes prompt

--- lib/svc/cdk/main.sh
+++ lib/svc/cdk/main.sh
@@ -12,10 +12,10 @@ p6_aws_cdk_prompt_info() {
 
     local str
     if ! p6_string_blank "$CDK_DEPLOY_ACCOUNT"; then
-      str=$(p6_string_append "$str" "AWS CDK: DEPLOY - [CDA=$CDK_DEPLOY_ACCOUNT] [CDR=$CDK_DEPLOY_REGION]")
+      str=$(p6_string_append "$str" "cdk:    DEPLOY - [CDA=$CDK_DEPLOY_ACCOUNT] [CDR=$CDK_DEPLOY_REGION]")
     fi
     if ! p6_string_blank "$CDK_DEFAULT_ACCOUNT"; then
-      str=$(p6_string_append "$str" "AWS CDK: DEFAULT - [CDA=$CDK_DEFAULT_ACCOUNT] [CDR=$CDK_DEFAULT_REGION]")
+      str=$(p6_string_append "$str" "cd:     DEFAULT - [CDA=$CDK_DEFAULT_ACCOUNT] [CDR=$CDK_DEFAULT_REGION]")
     fi
 
     if p6_string_blank "$str"; then
