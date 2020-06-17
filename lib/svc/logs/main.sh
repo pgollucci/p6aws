10ca2a53a8f04533e147b000102df3cfc54bc4a7
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-06-17 10:28:25 -0400

    fix(shellcheck): enable, fix (#19)

--- lib/svc/logs/main.sh
+++ lib/svc/logs/main.sh
@@ -1,15 +1,16 @@
+# shellcheck shell=sh
 ######################################################################
 #<
 #
 # Function: p6_aws_logs_svc_watch(log_group_name)
 #
 #  Args:
-#	log_group_name - 
+#	log_group_name -
 #
 #>
 ######################################################################
 p6_aws_logs_svc_watch() {
     local log_group_name="$1"
 
-    aws logs get $log_group_name ALL --watch
+    aws logs get "$log_group_name" ALL --watch
 }
