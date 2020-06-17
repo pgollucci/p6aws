e92a0bb47ad029afd1a0b89dbcc7c7c85ed70370
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-06-17 10:39:13 -0400

    feat(api): +p6_aws_logs_svc_watch_jq() (#20)

--- lib/svc/logs/main.sh
+++ lib/svc/logs/main.sh
@@ -14,3 +14,12 @@ p6_aws_logs_svc_watch() {
 
     aws logs get "$log_group_name" ALL --watch
 }
+
+p6_aws_logs_svc_watch_jq() {
+
+    p6_aws_logs_svc_watch "$@" |
+        while read -r line; do
+            json=$(echo "$line" | sed -e 's,^.*121789937014_CloudTrail_us-east-1 ,,')
+            echo "$json" | jq '.'
+        done
+}
