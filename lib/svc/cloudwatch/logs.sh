0d1c140a567bca8b6918e39c2afb1921f1680438
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-06-21 13:26:17 -0400

    fix(svc/logs): fixes typo in watch()

--- lib/svc/cloudwatch/logs.sh
+++ lib/svc/cloudwatch/logs.sh
@@ -12,7 +12,7 @@
 p6_aws_logs_svc_watch() {
     local log_group_name="$1"
 
-    aws logs get "$log_group_name" ALL --watch
+    awslogs get "$log_group_name" ALL --watch
 }
 
 ######################################################################
@@ -26,7 +26,9 @@ p6_aws_logs_svc_watch_jq() {
 
     p6_aws_logs_svc_watch "$@" |
         while read -r line; do
-            json=$(echo "$line" | sed -e 's,^.*121789937014_CloudTrail_us-east-1 ,,')
+            json=$(echo "$line" | sed -e 's,^.*_CloudTrail_us-east-1 ,,')
+            echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
             echo "$json" | jq '.'
+            echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
         done
 }
