98f476d9959f4f8f0d5f277b6a098795d3941533
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-07-07 18:23:11 -0400

    feat(cloudwatch): +p6_aws_logs_svc_trail_watch_jq(), +p6_aws_logs_svc_groups_list() (#25)

--- lib/svc/cloudwatch/logs.sh
+++ lib/svc/cloudwatch/logs.sh
@@ -23,12 +23,30 @@ p6_aws_logs_svc_watch() {
 #>
 ######################################################################
 p6_aws_logs_svc_watch_jq() {
+    local log_group_name="$1"
 
-    p6_aws_logs_svc_watch "$@" |
+    p6_aws_logs_svc_watch "$log_group_name" | \
         while read -r line; do
             json=$(echo "$line" | sed -e 's,^.*_CloudTrail_us-east-1 ,,')
             echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
             echo "$json" | jq '.'
             echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
         done
+}
+
+p6_aws_logs_svc_trail_watch_jq() {
+
+    local group
+    group=$(p6_aws_logs_svc_groups_list | grep TrailLogGroup | head -1)
+
+    p6_aws_logs_svc_watch "$group" | \
+    while read -r line; do
+        json=$(echo "$line" | sed -e 's,^.*_CloudTrail_us-east-1 ,,')
+        echo "$json" | jq '{time: .eventTime, name: .eventName, region: .awsRegion, from: .userIdentity.invokedBy, by: .eventSource, params: .requestParameters}'
+    done
+}
+
+p6_aws_logs_svc_groups_list() {
+
+    awslogs groups
 }
