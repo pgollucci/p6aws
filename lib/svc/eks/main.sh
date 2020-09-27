036a0f49d3ccd4b5715378bc69170a01f4e9deb7
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-09-27 17:08:31 -0400

    feat(api): +p6_aws_eks_svc_cluster_status()

--- lib/svc/eks/main.sh
+++ lib/svc/eks/main.sh
@@ -14,6 +14,8 @@ p6_aws_eks_svc_cluster_logging_enable() {
     p6_aws_cmd eks update-cluster-config \
         --name "$cluster_name" \
         --logging '{"clusterLogging":[{"types":["api","audit","authenticator","controllerManager","scheduler"],"enabled":true}]}'
+
+   p6_return_void
 }
 
 ######################################################################
@@ -30,7 +32,7 @@ p6_aws_eks_svc_kubeconfig_update() {
   local stack_name="$1"
 
   local code
-  code=$(aws cloudformation describe-stacks --stack-name "$stack_name"  --query 'Stacks[0].Outputs' | grep -A1 SmileEksConfigCommand |awk -F\" '/OutputValue/ { print $4 }')
+  code=$(p6_aws_cmd cloudformation describe-stacks --stack-name "$stack_name"  --query 'Stacks[0].Outputs' | grep -A1 SmileEksConfigCommand |awk -F\" '/OutputValue/ { print $4 }')
 
   p6_run_code "$code"
 
@@ -40,4 +42,31 @@ p6_aws_eks_svc_kubeconfig_update() {
   aws_eks_cluster_name=$(p6_echo "$P6_KUBE_CFG" | awk -F/ '{ print $2}')
 
   p6_env_export "AWS_EKS_CLUSTER_NAME" "$aws_eks_cluster_name"
+
+  p6_return_void
+}
+
+######################################################################
+#<
+#
+# Function: str status = p6_aws_eks_svc_cluster_status([cluster_name=$AWS_EKS_CLUSTER_NAME])
+#
+#  Args:
+#	OPTIONAL cluster_name -  [$AWS_EKS_CLUSTER_NAME]
+#
+#  Returns:
+#	str - status
+#
+#>
+######################################################################
+p6_aws_eks_svc_cluster_status() {
+  local cluster_name="${1:-$AWS_EKS_CLUSTER_NAME}"
+
+  local cluster_status
+  cluster_status=$(p6_aws_cmd eks describe-cluster \
+    --name "$cluster_name" \
+    --query "cluster.status" \
+    --output text)
+
+  p6_return_str "$cluster_status"
 }
