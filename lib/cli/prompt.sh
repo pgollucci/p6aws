ae55b63c4fe73cf7bd8f9bc4a73799f0cff517ce
Philip M. Gollucci <pgollucci@p6m7g8.com>
2021-03-16 08:42:49 -0400

    feat(prompt): adds org display

--- lib/cli/prompt.sh
+++ lib/cli/prompt.sh
@@ -14,13 +14,14 @@
 p6_aws_cfg_prompt_info() {
     local kind="$1"
 
+    local org=$(p6_aws_env_org"${kind}")
     local profile=$(p6_aws_env_profile"${kind}")
     local region=$(p6_aws_env_region"${kind}")
     local vpc_id=$(p6_aws_env_vpc_id"${kind}")
 
     local str
-    if ! p6_string_blank "$profile"; then
-        local str="aws:\t  ${kind}:$profile [$region] ($vpc_id)"
+    if ! p6_string_blank "$org"; then
+        local str="aws:\t  [$org] - ${kind}:$profile [$region] ($vpc_id)"
     fi
 
     p6_return_str "$str"
