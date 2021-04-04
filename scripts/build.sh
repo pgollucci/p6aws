045739559f38160ad9a714b31b843a8520748386
Philip M. Gollucci <pgollucci@p6m7g8.com>
2021-04-04 17:14:53 -0400

    try

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-. lib/_bootstrap.sh; p6_bootstrap "." "github"; p6_cicd_build_run
+if [ ! -d ../p6common ]; then
+  git clone https://github.com/p6m7g8/p6common ../
+  . ../p6common/lib/_bootstrap.sh
+   p6_bootstrap "." "github"
+   p6_cicd_build_run
+else
+  p6_bootstrap "." 
+  p6_cicd_build_run
+fi
